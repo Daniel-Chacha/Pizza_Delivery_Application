fa9d5f8f10a7b343fc91e1901001f82416eebad9
Daniel-Chacha <danmwita355@gmail.com>
2025-06-13 14:48:34 +0300

    added environment variables

--- pizza_delivery_app/firebaseConfig.js
+++ pizza_delivery_app/firebaseConfig.js
@@ -4,13 +4,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAs1hNhusvgVOvSsxiwwqFKs5ocJAURNAs",
-  authDomain: "pizza-inn-3f09a.firebaseapp.com",
-  projectId: "pizza-inn-3f09a",
-  storageBucket: "pizza-inn-3f09a.firebasestorage.app",
-  messagingSenderId: "239519249838",
-  appId: "1:239519249838:web:8c49be3aaf29b5fc1a043f",
-  measurementId: "G-6H8DFC6ZHJ"
+  apiKey:  process.env.APIKEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 export default firebaseConfig;
